841a4b91dc7043c70be0ca81dfd7165a2665ea6f
xavero <flavio.xavero@gmail.com>
2021-02-17 18:29:47 -0300

    feat: grpc-web example using Promise

--- demos/grpc-browser/src/index.js
+++ demos/grpc-browser/src/index.js
@@ -1,22 +1,22 @@
 import { HelloRequest } from './grpc/greet_pb.js';
-import { GreeterClient } from './grpc/greet_grpc_web_pb.js';
+import { GreeterPromiseClient } from './grpc/greet_grpc_web_pb.js';
 
-const echoService = new GreeterClient('https://localhost:5001');
+const greetService = new GreeterPromiseClient('https://localhost:5001');
 
-document.getElementById('sendCommand').addEventListener('click', () => {
+document.getElementById('sendCommand').addEventListener('click', async () => {
 
     let nameText = document.getElementById('requestText').value || 'John Doe';
 
     const request = new HelloRequest();
 
     request.setName(nameText);
-    
-    echoService.sayHello(request, {}, (err, response) => {
-        const responseText = document.getElementById('responseText');
-        if (err) {
-            responseText.innerText = `Error ${err.code} - ${err.message}`;
-        } else {
-            responseText.innerText = `Response: ${response.getMessage()}`;
-        }
-    });
+
+    const responseText = document.getElementById('responseText');
+
+    try {
+        const response = await greetService.sayHello(request);
+        responseText.innerText = `Response: ${response.getMessage()}`;
+    } catch (err) {
+        responseText.innerText = `Error ${err.code} - ${err.message}`;
+    }
 });
